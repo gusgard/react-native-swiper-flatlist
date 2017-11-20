465f74711a0be8301a516db732ff330c128ce807
Gustavo Gard <gusgard@gmail.com>
2017-11-19 23:40:49 -0300

    remove default prop

--- src/components/SwiperFlatList/test.js
+++ src/components/SwiperFlatList/test.js
@@ -1,12 +1,20 @@
 import { shallow } from 'enzyme';
 import React from 'react';
+import { Image } from 'react-native';
 
 import SwiperFlatList from './index';
 
 const logo = { uri: 'https://...' };
-const items = [{ id: 1, picture: logo }, { id: 2, picture: logo }];
+const items = [{ id: 1, thumbnail: logo }, { id: 2, thumbnail: logo }];
 
-it('renders correctly', () => {
-  const wrapper = shallow(<SwiperFlatList data={items} />);
-  expect(wrapper).toMatchSnapshot();
+describe('swiper flatlist', () => {
+  it('renders correctly', () => {
+    const wrapper = shallow(
+      <SwiperFlatList
+        renderItem={({ item }) => <Image source={item.thumbnail} />}
+        data={items}
+      />,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
 });
