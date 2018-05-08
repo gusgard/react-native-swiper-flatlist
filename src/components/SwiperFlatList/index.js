624ba85099fed00797518f3bc55ec2c283080eaa
Jeff Gabriel <jgabriel@SALT-100039.local>
2018-05-08 11:57:20 -0600

    index param for onMomentumScrollEnd

--- src/components/SwiperFlatList/index.js
+++ src/components/SwiperFlatList/index.js
@@ -140,7 +140,7 @@ export default class SwiperFlatList extends PureComponent {
     this.setState({ paginationIndex: index });
 
     if (onMomentumScrollEnd) {
-      onMomentumScrollEnd();
+      onMomentumScrollEnd(index);
     }
   };
 
