9c5647ee36cedc9bafa267884470abe6cec0c36b
Gustavo Gard <gusgard@gmail.com>
2019-08-09 23:16:32 -0300

    clean code

--- example/e2e/firstTest.spec.js
+++ example/e2e/firstTest.spec.js
@@ -1,9 +1,6 @@
 const config = require('../package.json').detox;
 describe('Example', () => {
   beforeEach(async () => {
-    // if (typeof device == 'undefined') {
-    //   await detox.init(config);
-    // }
     await device.reloadReactNative();
   });
 
