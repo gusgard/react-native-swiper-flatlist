8227768af5dacae0d3c8f5607bfe42759cae59b1
Enieber Cunha <eniebercunha@gmail.com>
2020-12-12 02:14:00 -0300

    fix: restore test

--- src/components/Pagination/test.js
+++ src/components/Pagination/test.js
@@ -1,99 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { StyleSheet, TouchableOpacity, View, ViewPropTypes } from 'react-native';
+import { render } from 'react-native-testing-library';
 
-import { colors, vertical, horizontal } from '../../themes';
+import Pagination from './index';
 
-const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    flexDirection: 'row',
-    marginVertical: vertical.xxSmall,
-    justifyContent: 'center',
-    alignItems: 'center',
-    bottom: 0,
-    width: '100%',
-  },
-  pagination: {
-    bottom: 0,
-    width: 8,
-    height: 8,
-    borderRadius: 4,
-    marginLeft: 3,
-    marginRight: 3,
-    marginTop: 3,
-  },
+describe('pagination', () => {
+  it('renders correctly', () => {
+    const { toJSON } = render(<Pagination scrollToIndex={() => undefined} size={5} />);
+    expect(toJSON()).toMatchSnapshot();
+  });
+  it('renders all props', () => {
+    const { toJSON } = render(
+      <Pagination
+        scrollToIndex={() => undefined}
+        paginationIndex={1}
+        paginationActiveColor="black"
+        paginationDefaultColor="white"
+        size={5}
+      />,
+    );
+    expect(toJSON()).toMatchSnapshot();
+  });
 });
-
-const Pagination = ({
-  size,
-  paginationIndex,
-  scrollToIndex,
-  paginationDefaultColor,
-  paginationActiveColor,
-  paginationStyle,
-  paginationStyleItem,
-  renderRightButton,
-  renderLeftButton,
-  hasButton,
-}) => {
-  if (hasButton) {
-    return (
-      <View style={[styles.container, paginationStyle]}>
-        {renderLeftButton()}
-        {Array.from({ length: size }).map((_, index) => (
-              <View
-                  style={[
-                    styles.pagination,
-                    paginationStyleItem,
-                    paginationIndex === index
-                      ? { backgroundColor: paginationActiveColor }
-                      : { backgroundColor: paginationDefaultColor },
-                  ]}
-                  key={index}
-                  onPress={() => scrollToIndex({ index })}
-                />
-        ))}
-        {renderRightButton()}
-      </View>
-    )
-  }
-  return (
-    <View style={[styles.container, paginationStyle]}>
-      {Array.from({ length: size }).map((_, index) => (
-        <TouchableOpacity
-          style={[
-            styles.pagination,
-            paginationStyleItem,
-            paginationIndex === index
-              ? { backgroundColor: paginationActiveColor }
-              : { backgroundColor: paginationDefaultColor },
-          ]}
-          key={index}
-          onPress={() => scrollToIndex({ index })}
-        />
-      ))}
-    </View>
-  );
-};
-Pagination.propTypes = {
-  scrollToIndex: PropTypes.func.isRequired,
-  size: PropTypes.number.isRequired,
-  paginationIndex: PropTypes.number,
-  paginationActiveColor: PropTypes.string,
-  paginationDefaultColor: PropTypes.string,
-  paginationStyle: ViewPropTypes.style,
-  paginationStyleItem: ViewPropTypes.style,
-  renderRightButton: PropTypes.func,
-  renderLeftButton: PropTypes.func,
-};
-
-Pagination.defaultProps = {
-  paginationIndex: 0,
-  paginationActiveColor: colors.white,
-  paginationDefaultColor: colors.gray,
-  paginationStyle: {},
-  paginationStyleItem: {},
-};
-
-export default Pagination;
