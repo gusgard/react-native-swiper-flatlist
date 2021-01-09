07d3e3c545960670ed576c3244121ec6e98921a5
Gustavo Gard <gusgard@gmail.com>
2021-01-09 10:42:39 -0300

    rename variables

--- example/e2e/renderItems.e2e.js
+++ example/e2e/renderItems.e2e.js
@@ -1,25 +1,25 @@
 const { device, expect, element, by } = require('detox');
 
 // const TEXT_IN_SCREENS = {
-//   ZERO: 'Item at index 0',
-//   ONE: 'Item at index 1',
-//   TWO: 'Item at index 2',
-//   THREE: 'Item at index 3',
-//   FOURTH: 'Item at index 4',
+//   zero: 'Item at index 0',
+//   one: 'Item at index 1',
+//   two: 'Item at index 2',
+//   three: 'Item at index 3',
+//   fourth: 'Item at index 4',
 // };
 const SCREENS = {
-  ZERO: 'container_swiper_renderItem_screen_0',
-  ONE: 'container_swiper_renderItem_screen_1',
-  TWO: 'container_swiper_renderItem_screen_2',
-  THREE: 'container_swiper_renderItem_screen_3',
-  FOURTH: 'container_swiper_renderItem_screen_4',
+  zero: 'container_swiper_renderItem_screen_0',
+  one: 'container_swiper_renderItem_screen_1',
+  two: 'container_swiper_renderItem_screen_2',
+  three: 'container_swiper_renderItem_screen_3',
+  fourth: 'container_swiper_renderItem_screen_4',
 };
 // const PAGINATION = {
-//   ZERO: 'container_swiper_renderItem_pagination_0',
-//   ONE: 'container_swiper_renderItem_pagination_1',
-//   TWO: 'container_swiper_renderItem_pagination_2',
-//   THREE: 'container_swiper_renderItem_pagination_3',
-//   FOURTH: 'container_swiper_renderItem_pagination_4',
+//   zero: 'container_swiper_renderItem_pagination_0',
+//   one: 'container_swiper_renderItem_pagination_1',
+//   two: 'container_swiper_renderItem_pagination_2',
+//   three: 'container_swiper_renderItem_pagination_3',
+//   fourth: 'container_swiper_renderItem_pagination_4',
 // };
 
 const DELAY = 2 * 1000;
@@ -27,7 +27,7 @@ const DELAY = 2 * 1000;
 describe('example with renderItems and data', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
-    // TODO: add menu with more steps!
+    // TODO: add menu with steps!
     // await element(by.id('renderItem')).tap();
   });
 
@@ -36,48 +36,48 @@ describe('example with renderItems and data', () => {
   });
 
   it('should execute autoplay every 2.0s', async () => {
-    // Initial with first swiper
-    await expect(element(by.id(SCREENS.ZERO))).toBeVisible();
-    await expect(element(by.id(SCREENS.ONE))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.TWO))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.THREE))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.FOURTH))).toBeNotVisible();
+    // 0s => screen with zero
+    await expect(element(by.id(SCREENS.zero))).toBeVisible();
+    await expect(element(by.id(SCREENS.one))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.two))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.three))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.fourth))).toBeNotVisible();
 
-    // after 3s => second swiper
+    // after 2s => screen with one
     await new Promise((_) => setTimeout(_, DELAY));
 
-    await expect(element(by.id(SCREENS.ZERO))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.ONE))).toBeVisible();
-    await expect(element(by.id(SCREENS.TWO))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.THREE))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.FOURTH))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.zero))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.one))).toBeVisible();
+    await expect(element(by.id(SCREENS.two))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.three))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.fourth))).toBeNotVisible();
 
-    // after 6s => third swiper
+    // after 4s => screen with two
     await new Promise((_) => setTimeout(_, DELAY));
 
-    await expect(element(by.id(SCREENS.ZERO))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.ONE))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.TWO))).toBeVisible();
-    await expect(element(by.id(SCREENS.THREE))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.FOURTH))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.zero))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.one))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.two))).toBeVisible();
+    await expect(element(by.id(SCREENS.three))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.fourth))).toBeNotVisible();
 
-    // after 9s => fourh swiper
+    // after 6s => screen with three
     await new Promise((_) => setTimeout(_, DELAY));
 
-    await expect(element(by.id(SCREENS.ZERO))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.ONE))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.TWO))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.THREE))).toBeVisible();
-    await expect(element(by.id(SCREENS.FOURTH))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.zero))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.one))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.two))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.three))).toBeVisible();
+    await expect(element(by.id(SCREENS.fourth))).toBeNotVisible();
 
-    // after 9s => five swiper
+    // after 8s => screen with fourth
     await new Promise((_) => setTimeout(_, DELAY));
 
-    await expect(element(by.id(SCREENS.ZERO))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.ONE))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.TWO))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.THREE))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.FOURTH))).toBeVisible();
+    await expect(element(by.id(SCREENS.zero))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.one))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.two))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.three))).toBeNotVisible();
+    await expect(element(by.id(SCREENS.fourth))).toBeVisible();
   });
 
   // https://github.com/wix/Detox/blob/master/docs/APIRef.ActionsOnElement.md#swipedirection-speed-percentage
