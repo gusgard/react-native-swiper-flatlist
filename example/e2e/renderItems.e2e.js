a9e5525404130cbe78e1f6cb156560157a18c537
Gustavo Gard <gusgard.it@gmail.com>
2021-01-18 11:56:23 -0300

    Update renderItems.e2e.js

--- example/e2e/renderItems.e2e.js
+++ example/e2e/renderItems.e2e.js
@@ -37,47 +37,52 @@ describe('example with renderItems and data', () => {
 
   it('should execute autoplay every 2.0s', async () => {
     // 0s => screen with zero
-    await expect(element(by.id(SCREENS.zero))).toBeVisible();
+    await waitFor(element(by.id(SCREENS.zero))).toBeVisible().withTimeout(DELAY);
+    // await expect(element(by.id(SCREENS.zero))).toBeVisible();
     await expect(element(by.id(SCREENS.one))).toBeNotVisible();
     await expect(element(by.id(SCREENS.two))).toBeNotVisible();
     await expect(element(by.id(SCREENS.three))).toBeNotVisible();
     await expect(element(by.id(SCREENS.fourth))).toBeNotVisible();
 
     // after 2s => screen with one
-    await new Promise((_) => setTimeout(_, 2500));
+    // await new Promise((_) => setTimeout(_, 2500));
 
+    await waitFor(element(by.id(SCREENS.one))).toBeVisible().withTimeout(DELAY);
     await expect(element(by.id(SCREENS.zero))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.one))).toBeVisible();
+    // await expect(element(by.id(SCREENS.one))).toBeVisible();
     await expect(element(by.id(SCREENS.two))).toBeNotVisible();
     await expect(element(by.id(SCREENS.three))).toBeNotVisible();
     await expect(element(by.id(SCREENS.fourth))).toBeNotVisible();
 
     // after 4s => screen with two
-    await new Promise((_) => setTimeout(_, DELAY));
+    // await new Promise((_) => setTimeout(_, DELAY));
 
+    await waitFor(element(by.id(SCREENS.two))).toBeVisible().withTimeout(DELAY);
     await expect(element(by.id(SCREENS.zero))).toBeNotVisible();
     await expect(element(by.id(SCREENS.one))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.two))).toBeVisible();
+    // await expect(element(by.id(SCREENS.two))).toBeVisible();
     await expect(element(by.id(SCREENS.three))).toBeNotVisible();
     await expect(element(by.id(SCREENS.fourth))).toBeNotVisible();
 
     // after 6s => screen with three
-    await new Promise((_) => setTimeout(_, DELAY));
+    // await new Promise((_) => setTimeout(_, DELAY));
 
+    await waitFor(element(by.id(SCREENS.three))).toBeVisible().withTimeout(DELAY);
     await expect(element(by.id(SCREENS.zero))).toBeNotVisible();
     await expect(element(by.id(SCREENS.one))).toBeNotVisible();
     await expect(element(by.id(SCREENS.two))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.three))).toBeVisible();
+    // await expect(element(by.id(SCREENS.three))).toBeVisible();
     await expect(element(by.id(SCREENS.fourth))).toBeNotVisible();
 
     // after 8s => screen with fourth
-    await new Promise((_) => setTimeout(_, DELAY));
+    // await new Promise((_) => setTimeout(_, DELAY));
 
+    await waitFor(element(by.id(SCREENS.fourth))).toBeVisible().withTimeout(DELAY);
     await expect(element(by.id(SCREENS.zero))).toBeNotVisible();
     await expect(element(by.id(SCREENS.one))).toBeNotVisible();
     await expect(element(by.id(SCREENS.two))).toBeNotVisible();
     await expect(element(by.id(SCREENS.three))).toBeNotVisible();
-    await expect(element(by.id(SCREENS.fourth))).toBeVisible();
+    // await expect(element(by.id(SCREENS.fourth))).toBeVisible();
   });
 
   // https://github.com/wix/Detox/blob/master/docs/APIRef.ActionsOnElement.md#swipedirection-speed-percentage
