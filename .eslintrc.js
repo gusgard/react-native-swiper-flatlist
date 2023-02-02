e2f702ae265c997b8fa7aec827b6f87a457eae53
Gustavo Gard <gusgard@gmail.com>
2023-02-02 18:24:34 -0300

    Upgrade example (#156)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,39 +3,8 @@ const WARNING = 1;
 const ERROR = 2;
 
 module.exports = {
-  extends: [
-    '@react-native-community',
-    // 'prettier',
-    // 'prettier/flowtype',
-    // 'prettier/react',
-    // 'eslint:recommended',
-    // 'plugin:import/errors',
-    // 'plugin:import/warnings',
-  ],
+  extends: ['@react-native-community'],
   plugins: ['detox'],
-  parserOptions: {
-    ecmaVersion: 2019,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  env: {
-    es6: true,
-    node: true,
-  },
-  rules: {
-    'consistent-return': WARNING,
-  },
-  settings: {
-    'import/resolver': {
-      reactnative: {},
-    },
-    react: {
-      pragma: 'React', // Pragma to use, default to "React"
-      version: '99', // React version, default to the latest React stable release
-    },
-  },
   globals: {
     it: false,
     describe: false,
--- example/index.js
+++ example/index.js
@@ -1,5 +1,9 @@
-import { AppRegistry } from 'react-native';
+/**
+ * @format
+ */
 
+import {AppRegistry} from 'react-native';
 import App from './src/App';
+import {name as appName} from './app.json';
 
-AppRegistry.registerComponent('SwiperFlatListExample', () => App);
+AppRegistry.registerComponent(appName, () => App);
