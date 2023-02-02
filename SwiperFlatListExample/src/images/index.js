16a7500390571be221e6422a0f3acbc50dc1a091
Gustavo Gard <gusgard@gmail.com>
2023-02-02 12:37:24 -0300

    Update project to RN 71.2

--- SwiperFlatListExample/src/images/index.js
+++
@@ -1,5 +0,0 @@
-export { default as fox } from './fox.jpeg';
-export { default as cat } from './cat.jpg';
-export { default as background } from './background.jpg';
-export { default as element } from './element.jpg';
-export { default as lion } from './lion.jpg';
--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -3,7 +3,7 @@ const OFF = 0;
 module.exports = {
   root: true,
   extends: '@react-native-community',
-    rules: {
+  rules: {
     'react-native/no-inline-styles': OFF,
   },
   globals: {
