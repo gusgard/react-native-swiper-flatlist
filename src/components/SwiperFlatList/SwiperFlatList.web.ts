b09015bceb50db23870c10579ec77d29c07c9d23
Gustavo Gard <gusgard@gmail.com>
2023-03-25 20:14:30 -0300

    Remove global.css

--- src/components/SwiperFlatList/SwiperFlatList.web.ts
+++ src/components/SwiperFlatList/SwiperFlatList.web.ts
@@ -1,7 +1,3 @@
 const { SwiperFlatList: SwiperFlatListBase } = require('./SwiperFlatList.tsx');
 
-try {
-  require('./global.css');
-} catch (error) {}
-
 export const SwiperFlatList = SwiperFlatListBase;
