39c2afb8f68fa1dc288d10053e08f4b9b2bda0ee
Gustavo Gard <gusgard@gmail.com>
2023-04-23 04:47:51 -0300

    remove useOnChangeIndex

--- example/.detoxrc.js
+++ example/.detoxrc.js
@@ -50,7 +50,7 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_3a_API_30_x86',
+        avdName: 'Pixel_4_API_33_arm64',
       },
     },
   },
