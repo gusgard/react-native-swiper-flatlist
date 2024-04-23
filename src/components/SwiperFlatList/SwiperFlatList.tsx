9b26891d58a02b51cc4ce5127f8c819b91a992f5
An Nguyễn Phước <31690470+phuocantd@users.noreply.github.com>
2024-04-23 10:05:36 +0700

    fix onChangeIndex iOS (#183)
    * fix callback onChangeIndex
    
    * fix change index android

--- src/components/SwiperFlatList/SwiperFlatList.tsx
+++ src/components/SwiperFlatList/SwiperFlatList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
   FlatList as RNFlatList,
   FlatListProps,
@@ -216,6 +216,10 @@ export const SwiperFlatList = React.forwardRef(
       [onViewableItemsChanged],
     );
 
+    const viewabilityConfigCallbackPairs = useRef<any>([
+      { onViewableItemsChanged:_onViewableItemsChanged, },
+    ]);
+
     const flatListProps: FlatListProps<unknown> & { ref: React.RefObject<RNFlatList<unknown>> } = {
       scrollEnabled,
       ref: flatListElement,
@@ -242,7 +246,8 @@ export const SwiperFlatList = React.forwardRef(
         itemVisiblePercentThreshold: ITEM_VISIBLE_PERCENT_THRESHOLD,
         ...viewabilityConfig,
       },
-      onViewableItemsChanged: _onViewableItemsChanged,
+      viewabilityConfigCallbackPairs: Platform.OS === 'ios'? viewabilityConfigCallbackPairs.current : undefined,
+      onViewableItemsChanged: Platform.OS === 'android'? _onViewableItemsChanged : undefined,
       // debug: true, // for debug
       testID: e2eID,
     };
